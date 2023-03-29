3198152f93abed6d674e7a6da4edeee71278e081
DUDA <mariabevenuto062@gmail.com>
2023-03-29 15:48:55 -0300

    feat: test another sticker

--- src/App.java
+++ src/App.java
@@ -5,9 +5,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         /* set sticker properties */
         Sticker sticker = new Sticker();
-        sticker.setStickerName("duck");
-        sticker.setStickerText("cute duck!");
-        sticker.setUrl("https://www.petz.com.br/blog/wp-content/uploads/2021/08/pato-de-estimacao2.jpg");
+        sticker.setStickerName("ittle duck");
+        sticker.setStickerText("cute baby duck!");
+        sticker.setUrl("https://media.istockphoto.com/id/187217267/pt/foto/pato-beb%C3%A9.jpg?s=1024x1024&w=is&k=20&c=yHxzgfLhb2ucNTPDfjYNBkA_yTcvk78vWcqMy7U62x8=");
 
         /* build sticker with stickerMaker */
         InputStream inputStream = new URL(sticker.getUrl()).openStream();
